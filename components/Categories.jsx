8978f5c18bbd2087c6d17893635ee049b0f0bc03
Emilio <emiliodev1986@gmail.com>
2022-05-05 19:42:05 -0400

    first commit graphcms, PostCard, PostWidget

---
+++ components/Categories.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
+
+const Categories = () => {
+  return (
+    <div>
+      Categories
+    </div>
+  )
+}
+
+export default Categories
---
+++ components/PostWidget.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [slug]);
+
+  console.log(relatedPosts.length);
+  console.log(relatedPosts);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => {
+        return (
+          <div key={post.title} className="flex items-center w-full mb-4">
+            <div className="w-16 flex-none">
+              <img
+                alt={post.title}
+                height="60px"
+                width="60px"
+                className="align-middle rounded-full"
+                src={post.featuredImage.url}
+              />
+            </div>
+            <div className="flex-grow ml-4">
+              <p className="yext-gray-500 font-xs">
+                {moment(post.createdAt).format("MMM DD, YYYY")}
+              </p>
+            </div>
+            <Link
+              className="text-md"
+              href={`/post/${post.slug}`}
+              key={post.title}
+            >
+              {post.title}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PostWidget;
